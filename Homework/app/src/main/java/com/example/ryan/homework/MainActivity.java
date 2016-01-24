7218ef8bcf53adcbc5dea3fcfeaf61667293da84
SangHakLee <leesanghak7298@gmail.com>
2016-01-25 00:52:58 +0900

    수정 완료

--- Homework/app/src/main/java/com/example/ryan/homework/MainActivity.java
+++ Homework/app/src/main/java/com/example/ryan/homework/MainActivity.java
@@ -39,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.btn_add :
                     addListBtn();
                     break;
+                case R.id.btn_edit :
+                    editListBtn();
+                    break;
             }
         }
     };
@@ -46,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     String name;
     String price;
     int price_int;
-    int selectedRadio; // 선택된 라디오 Id
+//    int selectedRadio; // 선택된 라디오 Id
     int radio_type; // 선탣된 값
 
     void addListBtn(){
@@ -56,21 +59,31 @@ public class MainActivity extends AppCompatActivity {
 //            LogManager.logPrint("값 안채워짐");
             return;
         }else{
-            name = et_name.getText().toString();
-            price = et_price.getText().toString();
-            radio_type = getRadio(radioGroup.getCheckedRadioButtonId());
-            price_int = Integer.parseInt(price);
-            LogManager.logPrint(name +", "+ price + ", " + radio_type);
-
-            // 여기부터 add
-            Item item = new Item(name, price_int, radio_type);
-            list.add(item);
+//            name = et_name.getText().toString();
+//            price = et_price.getText().toString();
+//            radio_type = getRadio(radioGroup.getCheckedRadioButtonId());
+//            price_int = Integer.parseInt(price);
+//            LogManager.logPrint(name +", "+ price + ", " + radio_type);
+//
+//            // 여기부터 add
+//            Item item = new Item(name, price_int, radio_type);
+            list.add(addItem());
             adapter.notifyDataSetChanged(); // UI 변경 알리기
-
             listView.smoothScrollToPosition(adapter.getCount()-1); // 추가하고 해당 위치로
         }
     }
 
+    public Item addItem(){
+        name = et_name.getText().toString();
+        price = et_price.getText().toString();
+        radio_type = getRadio(radioGroup.getCheckedRadioButtonId());
+        price_int = Integer.parseInt(price);
+
+        Item item = new Item(name, price_int, radio_type);
+      return item;
+    };
+
+    // 라디오 버튼 값 가져오기
     public int getRadio(int RadioButtonId){
         int type = TYPE_A;
         switch (RadioButtonId){
@@ -87,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
         return type;
     };
 
+    // 추가하는 값 유효성 검사
     public Boolean checkIsEditTextValid(){ // 에딧 텍스트 입력갑 확인
         if( et_name.getText().toString().getBytes().length <= 0 ){
 //            LogManager.logPrint("이름 없음");
@@ -99,8 +113,17 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
-    public void selectListItem(){
+    private int nowListPosition = -1;
 
+    void editListBtn(){
+        LogManager.logPrint(" " + nowListPosition);
+        if( nowListPosition < 0){
+            Toast.makeText(this, "수정할 리스트를 먼저 클릭해주세요.", Toast.LENGTH_SHORT).show();
+        }else{
+            list.set(nowListPosition, addItem());
+            adapter.notifyDataSetChanged(); // UI 변경 알리기
+            listView.smoothScrollToPosition(adapter.getCount()-1); // 추가하고 해당 위치로
+        }
     }
 
 
@@ -110,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.btn_add).setOnClickListener(handler);
+        findViewById(R.id.btn_edit).setOnClickListener(handler);
         et_name = (EditText)findViewById(R.id.et_name);
         et_price = (EditText)findViewById(R.id.et_price);
 
@@ -129,6 +153,9 @@ public class MainActivity extends AppCompatActivity {
                 et_name.setText(list.get(position).name);
                 et_price.setText(list.get(position).price + "");
                 findViewById(R.id.btn_edit).setVisibility(View.VISIBLE);
+
+                nowListPosition = position; // 수정할 수 있게 현재 위치를 넘김
+
 //                LogManager.logPrint("aaaa");
             }
         });
